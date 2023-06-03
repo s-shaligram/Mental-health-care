257d612767940d7e08357d59bcbe55e33ac5279a
Sameer_Shaligram <s_shaligram@fanshaweonline.ca>
2023-06-03 18:02:57 -0400

    mood track code added

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
+import MoodTracker from './MoodTrack/moodtracker';
 
 export default function App() {
 
-  SplashScreen.preventAutoHideAsync()
-  .then((prevented) => {
-    console.log('Prevented:', prevented);
-  })
-  .catch((error) => {
-    console.log('Prevent error:', error);
-  });
+  // SplashScreen.preventAutoHideAsync()
+  // .then((prevented) => {
+  //   console.log('Prevented:', prevented);
+  // })
+  // .catch((error) => {
+  //   console.log('Prevent error:', error);
+  // });
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
       <StatusBar style="auto" />
+      <MoodTracker/>
     </View>
   );
 }
