1ec4fd8f22f08d0d4656084ede28588f52808878
GhanashyamShingate <g_shingate@fanshaweonline.ca>
2023-06-03 19:51:59 -0400

    Added Count Game-Basic

--- src/screens/Active/index.js
+++ src/screens/Active/index.js
@@ -1,43 +1,63 @@
-import React from "react";
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+// import React from "react";
+// import { View, Text, StyleSheet, ScrollView } from "react-native";
 
-function Home() {
-    return (
-        <ScrollView>
-            <View style={styles.container}>
-                <View style={styles.line} />
-                <Text style={styles.title}>Text 01</Text>
-                <View style={styles.line} />
-            </View>
+// function Home() {
+//   return (
+//     <ScrollView>
+//       <View style={styles.container}>
+//         <View style={styles.line} />
+//         <Text style={styles.title}>Hi</Text>
+//         <View style={styles.line} />
+//       </View>
 
-            <View style={styles.container}>
-                <View style={styles.line} />
-                <Text style={styles.title}>Text 02</Text>
-                <View style={styles.line} />
-            </View>
-        </ScrollView>
-    );
+//       <View style={styles.container}>
+//         <View style={styles.line} />
+//         <Text style={styles.title}>Hello</Text>
+//         <View style={styles.line} />
+//       </View>
+//     </ScrollView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flexDirection: "row",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     marginVertical: 10,
+//   },
+//   line: {
+//     flex: 1,
+//     height: 1,
+//     backgroundColor: "#ccc",
+//     marginHorizontal: 10,
+//   },
+//   title: {
+//     fontSize: 18,
+//     fontWeight: "bold",
+//     color: "#555",
+//     marginHorizontal: 10,
+//   },
+// });
+
+// export default Home;
+import React, { useState } from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import styles from "./style";
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Count: {count}</Text>
+      <TouchableOpacity onPress={incrementCount} style={styles.button}>
+        <Text style={styles.buttonText}>Increment</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-        marginVertical: 10,
-    },
-    line: {
-        flex: 1,
-        height: 1,
-        backgroundColor: "#ccc",
-        marginHorizontal: 10,
-    },
-    title: {
-        fontSize: 18,
-        fontWeight: "bold",
-        color: "#555",
-        marginHorizontal: 10,
-    },
-});
-
-export default Home;
