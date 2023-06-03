eae7501d623dbf9af0f97504ca7fa8d69a7a475e
Shiwantha Viraj <s_wanniarachchige@fanshaweonline.ca>
2023-06-03 19:44:20 -0400

    test new

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import {NavigationContainer} from "@react-navigation/native";
 import Tabs from "./navigation/tabs";
 
-// dummy changes
+// dummy changes new new
 
 export default function App() {
 
