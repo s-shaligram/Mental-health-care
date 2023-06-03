751cf29dee49410d232ebae979e67ec62be46053
KavinRanawella <kavin@memaree.com>
2023-06-03 17:50:41 -0400

    adding bottom navigation

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import Tabs from "./navigation/tabs";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>App.js!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Tabs></Tabs>
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
