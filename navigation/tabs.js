4059f54fad1fba84943d9d50aa1e8c9fd767ca55
Kavin Ranawella <kavin256@gmail.com>
2023-06-04 12:45:23 -0400

    changing bottom navigation styles

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -1,5 +1,6 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {Text, View, TouchableOpacity} from "react-native";
+import Icon from 'react-native-vector-icons/Ionicons';
 import Home from "../src/screens/Home";
 import More from "../src/screens/More";
 import Active from "../src/screens/Active";
@@ -8,206 +9,176 @@ import Track from "../src/screens/Track";
 
 const Tab = createBottomTabNavigator();
 
-const CustomTabBarButton = ({ children, onPress }) => (
-  <TouchableOpacity
-    style={{
-      justifyContent: "center",
-      alignContent: "center",
-      ...styles.shadow,
-    }}
-    onPress={onPress}
-  >
-    <View
-      style={{
-        width: 70,
-        height: 70,
-        borderRadius: 35,
-        backgroundColor: "#0A7BF8",
-      }}
-    >
-      {children}
-    </View>
-  </TouchableOpacity>
-);
-
 const Tabs = () => {
-  return (
-    <Tab.Navigator initialRouteName="Home"
-      screenOptions={{
-        tabBarShowLabel: false,
-        tabBarStyle: {
-          position: "absolute",
-          bottom: 25,
-          left: 25,
-          right: 25,
-          elevation: 0,
-          backgroundColor: "#ffffff",
-          borderRadius: 15,
-          height: 80,
-          ...styles.shadow,
-        },
-      }}
-    >
-      <Tab.Screen
-        name="Active"
-        component={Active}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <View
-              style={{
-                alignItems: "center",
-                justifyContent: "center",
-                top: 15,
-              }}
-            >
-              <Image
-                resizeMode="contain"
-                style={{
-                  width: 25,
-                  height: 25,
-                  tintColor: focused ? "#0A7BF8" : "#555",
-                }}
-              />
-              <Text
-                style={{
-                  color: focused ? "#0A7BF8" : "#555",
-                  fontSize: 12,
-                }}
-              >Active
-              </Text>
-            </View>
-          ),
-        }}
-      />
-        <Tab.Screen
-            name="Calm"
-            component={Calm}
-            options={{
-                tabBarIcon: ({ focused }) => (
-                    <View
-                        style={{
-                            alignItems: "center",
-                            justifyContent: "center",
-                            top: 15,
-                        }}
-                    >
-                        <Image
-                            resizeMode="contain"
+    return (
+        <Tab.Navigator initialRouteName="Home"
+                       screenOptions={{
+                           tabBarShowLabel: false,
+                           tabBarStyle: {
+                               position: "absolute",
+                               // bottom: 10,
+                               // left: 25,
+                               // right: 25,
+                               // elevation: 0,
+                               backgroundColor: "#ffffff",
+                               // borderRadius: 15,
+                               height: 60,
+                           },
+                       }}
+        >
+            <Tab.Screen
+                name="Active"
+                component={Active}
+                options={{
+                    tabBarIcon: ({focused}) => (
+                        <View
                             style={{
-                                width: 25,
-                                height: 25,
-                                tintColor: focused ? "#0A7BF8" : "#555",
-                            }}
-                        />
-                        <Text
-                            style={{
-                                color: focused ? "#0A7BF8" : "#555",
-                                fontSize: 12,
+                                alignItems: "center",
+                                justifyContent: "center",
                             }}
                         >
-                            Calm
-                        </Text>
-                    </View>
-                ),
-            }}
-        />
-        <Tab.Screen
-            name="Home"
-            component={Home}
-            options={{
-                tabBarIcon: ({ focused }) => (
-                    <Image
-                        resizeMode="contain"
-                        style={{
-                            width: 40,
-                            height: 40,
-                            tintColor: focused ? "#fff" : "#fff",
-                        }}
-                    />
-                ),
-                tabBarButton: (props) => <CustomTabBarButton {...props} />,
-            }}
-        />
+                            <Icon
+                                name={focused ? "ios-flame" : "ios-flame-outline"}
+                                size={25}
+                                color={focused ? "#0A7BF8" : "#555"}
+                            />
+                            <Text
+                                style={{
+                                    color: focused ? "#0A7BF8" : "#555",
+                                    fontSize: 12,
+                                }}
+                            >Active
+                            </Text>
+                        </View>
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Calm"
+                component={Calm}
+                options={{
+                    tabBarIcon: ({focused}) => (
+                        <View
+                            style={{
+                                alignItems: "center",
+                                justifyContent: "center",
+                            }}
+                        >
+                            <Icon
+                                name={focused ? "ios-moon" : "ios-moon-outline"}
+                                size={25}
+                                color={focused ? "#0A7BF8" : "#555"}
+                            />
+                            <Text
+                                style={{
+                                    color: focused ? "#0A7BF8" : "#555",
+                                    fontSize: 12,
+                                }}
+                            >
+                                Calm
+                            </Text>
+                        </View>
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    tabBarIcon: ({focused}) => (
+                        <Icon
+                            name={focused ? "ios-home" : "ios-home-outline"}
+                            size={35}
+                            color={focused ? "#ffff" : "#555"}
+                        />
+                    ),
+                    tabBarButton: (props) => <CustomTabBarButton {...props} />,
+                }}
+            />
 
-        <Tab.Screen
-            name="Track"
-            component={Track}
-            options={{
-                tabBarIcon: ({ focused }) => (
-                    <View
-                        style={{
-                            alignItems: "center",
-                            justifyContent: "center",
-                            top: 15,
-                        }}
-                    >
-                        <Image
-                            resizeMode="contain"
+            <Tab.Screen
+                name="Track"
+                component={Track}
+                options={{
+                    tabBarIcon: ({focused}) => (
+                        <View
                             style={{
-                                width: 25,
-                                height: 25,
-                                tintColor: focused ? "#0A7BF8" : "#555",
-                            }}
-                        />
-                        <Text
-                            style={{
-                                color: focused ? "#0A7BF8" : "#555",
-                                fontSize: 12,
+                                alignItems: "center",
+                                justifyContent: "center",
                             }}
                         >
-                            Track
-                        </Text>
-                    </View>
-                ),
-            }}
-        />
-      <Tab.Screen
-        name="More"
-        component={More}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <View
-              style={{
-                alignItems: "center",
-                justifyContent: "center",
-                top: 15,
-              }}
-            >
-              <Image
-                resizeMode="contain"
-                style={{
-                  width: 25,
-                  height: 25,
-                  tintColor: focused ? "#0A7BF8" : "#555",
+                            <Icon
+                                name={focused ? "ios-compass" : "ios-compass-outline"}
+                                size={25}
+                                color={focused ? "#0A7BF8" : "#555"}
+                            />
+
+                            <Text
+                                style={{
+                                    color: focused ? "#0A7BF8" : "#555",
+                                    fontSize: 12,
+                                }}
+                            >
+                                Track
+                            </Text>
+                        </View>
+                    ),
                 }}
-              />
-              <Text
-                style={{
-                  color: focused ? "#0A7BF8" : "#555",
-                  fontSize: 12,
+            />
+            <Tab.Screen
+                name="More"
+                component={More}
+                options={{
+                    tabBarIcon: ({focused}) => (
+                        <View
+                            style={{
+                                alignItems: "center",
+                                justifyContent: "center",
+                            }}
+                        >
+                            <Icon
+                                name={focused ? "ios-menu" : "ios-menu-outline"}
+                                size={25}
+                                color={focused ? "#0A7BF8" : "#555"}
+                            />
+
+                            <Text
+                                style={{
+                                    color: focused ? "#0A7BF8" : "#555",
+                                    fontSize: 12,
+                                }}
+                            >
+                                More
+                            </Text>
+                        </View>
+                    ),
                 }}
-              >
-                  More
-              </Text>
-            </View>
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
+            />
+        </Tab.Navigator>
+    );
 };
 
-const styles = StyleSheet.create({
-  shadow: {
-    shadowColor: "#b0b0b0",
-    shadowOffset: {
-      width: 0,
-      height: 10,
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 3.5,
-    elevation: 5,
-  },
-});
-
 export default Tabs;
+
+const CustomTabBarButton = ({children, onPress}) => (
+    <TouchableOpacity
+        style={{
+            justifyContent: "center",
+            alignContent: "center",
+            margin: 15
+        }}
+        onPress={onPress}
+    >
+        <View
+            style={{
+                bottom: 6,
+                width: 70,
+                height: 70,
+                borderRadius: 35,
+                backgroundColor: "#0A7BF8",
+            }}
+        >
+            {children}
+        </View>
+    </TouchableOpacity>
+);
