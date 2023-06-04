da9d6997762b8fbe89bb03ae7810ccdb4c3aeb60
Sameer_Shaligram <s_shaligram@fanshaweonline.ca>
2023-06-04 17:43:56 -0400

    intial splash screen code added

--- App.js
+++ App.js
@@ -1,10 +1,54 @@
 import {NavigationContainer} from "@react-navigation/native";
 import Tabs from "./navigation/tabs";
+import * as SplashScreen from 'expo-splash-screen';
+import { Text, View } from "react-native";
+import React, { useCallback, useEffect, useState } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        
+        
+        await new Promise(resolve => setTimeout(resolve, 10000));
+
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+     
+      console.log('appIsReady and Splash Screen loading')
+      // await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-      <NavigationContainer>
+      <View 
+      style= { {
+        flex: 1,
+       } }
+       onLayout={onLayoutRootView}>
+        <NavigationContainer>
         <Tabs></Tabs>
-      </NavigationContainer>
+        </NavigationContainer>
+      </View>
   );
 }
