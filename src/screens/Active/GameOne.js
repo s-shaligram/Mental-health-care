440486f72f19ca69e34853f7e999c9b6bcdc1fe3
GhanashyamShingate <g_shingate@fanshaweonline.ca>
2023-06-04 14:13:17 -0400

    Added Random Text and Avatar Image

--- src/screens/Active/GameOne.js
+++ src/screens/Active/GameOne.js
@@ -1,21 +1,110 @@
+// import React, { useState } from "react";
+// import { View, Text, TouchableOpacity } from "react-native";
+// import styles from "./style";
+
+// export default function Active() {
+//   const [count, setCount] = useState(0);
+
+//   const incrementCount = () => {
+//     setCount(count + 1);
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.text}>Hello Ghanashyam</Text>
+//       <Text style={styles.text}>Count: {count}</Text>
+//       <TouchableOpacity onPress={incrementCount} style={styles.button}>
+//         <Text style={styles.buttonText}>Increment</Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// }
+
+// import React, { useState } from "react";
+// import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+// import styles from "./style";
+
+// export default function Home() {
+//   const [count, setCount] = useState(0);
+
+//   const incrementCount = () => {
+//     setCount(count + 1);
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.text}>Hello Ghanashyam</Text>
+//       <Text style={styles.text}>Count: {count}</Text>
+//       <TouchableOpacity onPress={incrementCount} style={styles.button}>
+//         <Text style={styles.buttonText}>Increment</Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// }
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity, Image } from "react-native";
 import styles from "./style";
 
-export default function Active() {
+const randomTexts = [
+  "Keep going!",
+  "You can do it",
+  "You're doing great!",
+  "Almost there!",
+  "Fantastic!",
+  "Nice work!",
+  "Calm Down",
+  "Let it Be",
+  "Time will Fly",
+  "You are awesome",
+];
+const avatars = [
+  require("/Users/ghanashyamshingate/Desktop/Fanshawe Level3/Capstone Project/Mental-health-care/assets/KeepGoing.png"),
+  require("/Users/ghanashyamshingate/Desktop/Fanshawe Level3/Capstone Project/Mental-health-care/assets/HappyEmotion.jpg"),
+  require("/Users/ghanashyamshingate/Desktop/Fanshawe Level3/Capstone Project/Mental-health-care/assets/fun.avif"),
+];
+const Active = () => {
   const [count, setCount] = useState(0);
+  const [randomIndex, setRandomIndex] = useState(0);
 
   const incrementCount = () => {
-    setCount(count + 1);
+    if (count < 10) {
+      setCount(count + 1);
+      setRandomIndex(Math.floor(Math.random() * randomTexts.length));
+    }
+  };
+  const renderAvatars = () => {
+    const avatarViews = [];
+
+    if (count > 0) {
+      const lastAvatarIndex = Math.min(count - 1, avatars.length - 1);
+
+      avatarViews.push(
+        <Image
+          key={lastAvatarIndex}
+          source={avatars[lastAvatarIndex]}
+          style={styles.avatar}
+          resizeMode="contain"
+        />
+      );
+    }
+
+    return avatarViews;
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello Ghanashyam</Text>
-      <Text style={styles.text}>Count: {count}</Text>
-      <TouchableOpacity onPress={incrementCount} style={styles.button}>
-        <Text style={styles.buttonText}>Increment</Text>
+      <View style={styles.avatarsContainer}>{renderAvatars()}</View>
+      <Text style={styles.count}>{count}</Text>
+      <Text style={styles.text}>{randomTexts[randomIndex]}</Text>
+      <TouchableOpacity
+        onPress={incrementCount}
+        disabled={count >= 10}
+        style={[styles.button, count >= 10 && styles.buttonDisabled]}
+      >
+        <Text style={styles.buttonText}>Press Me</Text>
       </TouchableOpacity>
     </View>
   );
-}
+};
+
+export default Active;
--- src/screens/Active/style.js
+++ src/screens/Active/style.js
@@ -9,8 +9,23 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginBottom: 20,
   },
+  count: {
+    fontSize: 70,
+    marginBottom: 40,
+    fontWeight: "bold",
+    color: "#1D741B", // Customize the color as per your preference
+    textShadowColor: "#000000",
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 2,
+  },
+  avatar: {
+    width: 150, // Customize the width as per your requirement
+    height: 150, // Customize the height as per your requirement
+    borderRadius: 25, // Customize the borderRadius as per your preference
+    marginBottom: 90,
+  },
   button: {
-    backgroundColor: "blue",
+    backgroundColor: "#FFA500",
     padding: 10,
     borderRadius: 5,
   },
