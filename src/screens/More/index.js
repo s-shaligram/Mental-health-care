7e2872e733341a4c82fefd85bb448a117bd44a0a
Shiwantha Viraj <s_wanniarachchige@fanshaweonline.ca>
2023-06-04 20:40:51 -0400

    merge conflicts

--- src/screens/More/index.js
+++ src/screens/More/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import NearByMedicalCenter from "../../components/nearByMedicalCenter";
+import NearByMedicalCenter from "../../components/NearByMedicalCenter/index";
 import MoreHome from "./MoreHome";
 const Stack = createStackNavigator();
 
