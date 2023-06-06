4ff3622b999755337bf69d09977a57335ee96b3c
Shiwantha Viraj <s_wanniarachchige@fanshaweonline.ca>
2023-06-05 20:30:24 -0400

    index.js file change

--- src/screens/More/index.js
+++ src/screens/More/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import NearByMedicalCenter from "../../components/NearByMedicalCenter/index";
+import NearByMedicalCenter from "../../components/NearByMedicalCenter";
 import MoreHome from "./MoreHome";
 const Stack = createStackNavigator();
 
