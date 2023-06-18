627b6bc6f236db03ff3bf4b8c22c0eefb8bbaa53
s-shaligram <s_shaligram@fanshaweonline.ca>
2023-06-18 17:33:49 -0400

    Initial code for mood tracking enhancement

--- src/components/MoodTrack/moodtracker.js
+++ src/components/MoodTrack/moodtracker.js
@@ -1,69 +1,73 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import styles from './styles';
 
 const MoodTracker = () => {
-  const [mood, setMood] = useState();
-
-  const handleMoodSelection = (selectedMood) => {
-    setMood(selectedMood);
-  };
-
+  const [mood, setMood] = useState(null);
   const [moodText, setMoodText] = useState('');
 
+  const handleMoodSelection = async (selectedMood) => {
+    setMood(selectedMood);
+    await saveMood(selectedMood);
+    handleMoodText(selectedMood);
+  };
+
   const handleMoodText = (selectedText) => {
-    
-    if(selectedText === '😄'){
-        setMoodText('Keep it up!! 😊');
-    } 
-    else if(selectedText === '😞'){
-        setMoodText('Everything will be fine, no need to worry!')
+    if (selectedText === '😄') {
+      setMoodText('Keep it up!! 😊');
+    } else if (selectedText === '😞') {
+      setMoodText('Everything will be fine, no need to worry!');
+    } else if (selectedText === '😠') {
+      setMoodText('Calm down and let it go. 🙂🙃');
+    } else if (selectedText === '😌') {
+      setMoodText('Peace ☮️');
     }
-    else if(selectedText === '😠'){
-        setMoodText('Calm down and let it go. 🙂🙃')
-    }
-    else if(selectedText === '😌'){
-        setMoodText('Peace ☮️')
+  };
+
+  const saveMood = async (selectedMood) => {
+    try {
+      const currentDate = new Date().toISOString().split('T')[0]; // Get the current date
+      const existingData = await AsyncStorage.getItem(currentDate); // Retrieve existing data for the current date
+
+      let newData = [];
+      if (existingData) {
+        newData = JSON.parse(existingData); // Parse existing data from JSON
+      }
+
+      newData.push({ mood: selectedMood }); // Add the new mood entry
+
+      await AsyncStorage.setItem(currentDate, JSON.stringify(newData)); // Save the updated data
+      console.log('Mood saved successfully.');
+    } catch (error) {
+      console.log('Error saving mood:', error);
     }
   };
 
   return (
     <View style={styles.container}>
-      
       <View style={styles.emojiContainer}>
         <TouchableOpacity
           style={styles.emoji}
-          onPress={() => {
-            handleMoodSelection('😄');
-            handleMoodText('😄');
-        }}
+          onPress={() => handleMoodSelection('😄')}
         >
           <Text style={styles.emojiText}>😄</Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.emoji}
-          onPress={() => {
-            handleMoodSelection('😞');
-            handleMoodText('😞');
-        }}
+          onPress={() => handleMoodSelection('😞')}
         >
-        <Text style={styles.emojiText}>😞</Text>
+          <Text style={styles.emojiText}>😞</Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.emoji}
-          onPress={() => {
-            handleMoodSelection('😠');
-            handleMoodText('😠');
-        }}
+          onPress={() => handleMoodSelection('😠')}
         >
           <Text style={styles.emojiText}>😠</Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.emoji}
-          onPress={() => {
-            handleMoodSelection('😌');
-            handleMoodText('😌');
-        }}
+          onPress={() => handleMoodSelection('😌')}
         >
           <Text style={styles.emojiText}>😌</Text>
         </TouchableOpacity>
