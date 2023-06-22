4629518a84e16037bc18fd3e5a3639e744d00165
p-rajan178310 <p_rajan178310@fanshaweonline.ca>
2023-06-22 02:15:14 -0400

    dark mode code for all screens

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
-import {NavigationContainer} from "@react-navigation/native";
+import {NavigationContainer, DarkTheme, DefaultTheme} from "@react-navigation/native";
 import Tabs from "./navigation/tabs";
 import * as SplashScreen from "expo-splash-screen";
-import {View} from "react-native";
+import {View, Appearance} from "react-native";
 import React, {useCallback, useEffect, useState} from "react";
 import {CommonProvider} from "./src/hooks/useGlobalContext";
-import { DarkModeProvider } from "./DarkModeContext";
+import { EventRegister } from 'react-native-event-listeners';
+import darkMode from './styles/darkMode';
+import themeContext from './styles/themeContext';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
     const [appIsReady, setAppIsReady] = useState(false);
+
+    const[theme, setTheme] = useState(false);
+
+    useEffect(() => {
+        const listener = EventRegister.addEventListener('ChangeTheme', (data) => {
+            setTheme(data);
+            console.log(data);
+        })
+
+        return ()=> {
+            EventRegister.removeAllListeners(listener);
+        }
+    }, [theme]);
 
     useEffect(() => {
         async function prepare() {
@@ -37,7 +52,6 @@ export default function App() {
     }
 
     return (
-        <DarkModeProvider>
         <View
             style={{
                 flex: 1,
@@ -45,11 +59,86 @@ export default function App() {
             onLayout={onLayoutRootView}
         >
             <CommonProvider>
-                <NavigationContainer>
-                    <Tabs></Tabs>
-                </NavigationContainer>
+                {/* <themeContext.Provider value={darkMode === true ? darkMode.dark : darkMode.light}>
+                    <NavigationContainer theme={darkMode === true ? DarkTheme : DefaultTheme}> */}
+                    <themeContext.Provider value={theme ? darkMode.dark : darkMode.light}>
+                    <NavigationContainer theme={theme ? DarkTheme : DefaultTheme}>
+
+                        <Tabs></Tabs>
+                    </NavigationContainer>
+                </themeContext.Provider>
             </CommonProvider>
         </View>
-        </DarkModeProvider>
     );
 }
+// import { createContext, useState, useEffect } from "react";
+// import { NavigationContainer, DarkTheme, DefaultTheme } from "@react-navigation/native";
+// import Tabs from "./navigation/tabs";
+// import * as SplashScreen from "expo-splash-screen";
+// import { View, Appearance } from "react-native";
+
+// const darkMode = {
+//   light: {
+//     theme: 'light',
+//     color: 'black',
+//     background: 'white'
+//   },
+//   dark: {
+//     theme: 'dark',
+//     color: 'white',
+//     background: 'black'
+//   }
+// };
+
+// const themeContext = createContext(darkMode.light);
+
+// export default function App() {
+//   const [appIsReady, setAppIsReady] = useState(false);
+//   const [theme, setTheme] = useState(false);
+
+//   useEffect(() => {
+//     const listener = Appearance.addChangeListener(({ colorScheme }) => {
+//       setTheme(colorScheme === 'dark');
+//     });
+
+//     return () => {
+//       Appearance.removeChangeListener(listener);
+//     };
+//   }, []);
+
+//   useEffect(() => {
+//     async function prepare() {
+//       try {
+//         await SplashScreen.preventAutoHideAsync();
+//         await new Promise((resolve) => setTimeout(resolve, 2000));
+//       } catch (e) {
+//         console.warn(e);
+//       } finally {
+//         // Tell the application to render
+//         setAppIsReady(true);
+//       }
+//     }
+
+//     prepare();
+//   }, []);
+
+//   const onLayoutRootView = async () => {
+//     if (appIsReady) {
+//       await SplashScreen.hideAsync();
+//     }
+//   };
+
+//   if (!appIsReady) {
+//     return null;
+//   }
+
+//   return (
+//     <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+//       <themeContext.Provider value={theme ? darkMode.dark : darkMode.light}>
+//         <NavigationContainer theme={theme ? DarkTheme : DefaultTheme}>
+//           <Tabs />
+//         </NavigationContainer>
+//       </themeContext.Provider>
+//     </View>
+//   );
+// }
