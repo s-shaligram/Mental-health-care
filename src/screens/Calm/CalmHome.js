4c91b965a4977918186ab857b277ec557bfed6f0
s-shaligram <s_shaligram@fanshaweonline.ca>
2023-06-22 16:31:11 -0400

    code updates

--- src/screens/Calm/CalmHome.js
+++ src/screens/Calm/CalmHome.js
@@ -16,20 +16,5 @@ function CalmHome({navigation}) {
 }
 
 export default CalmHome;
-// import React, { useContext } from "react";
-// import { ScrollView } from "react-native";
-// import SelectionTile from "../../components/SelectionTile";
-// import themeContext from "../../../styles/themeContext";
 
-// function CalmHome({ navigation }) {
-//   const theme = useContext(themeContext);
 
-//   return (
-//     <ScrollView style={{ backgroundColor: theme?.background }}>
-//       <SelectionTile name={"Change theme"} routeTo={'ScreenA'} navigation={navigation} />
-//       <SelectionTile name={"Selection B"} routeTo={'ScreenB'} navigation={navigation} />
-//     </ScrollView>
-//   );
-// }
-
-// export default CalmHome;
