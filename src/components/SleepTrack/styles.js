c2342becde5a75f80ed9eb5fc3e0cc6219d522cf
s-shaligram <s_shaligram@fanshaweonline.ca>
2023-06-23 01:18:18 -0400

    update mood tracking styles
    #50

--- src/components/SleepTrack/styles.js
+++ src/components/SleepTrack/styles.js
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
         width: 300,
         height: 20,
         backgroundColor: '#fff',
-
+        borderRadius: 10,
     },
     sliderValue: {
         marginTop: 10,
