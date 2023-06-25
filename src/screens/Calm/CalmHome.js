b555b107271f88c8d8bab7761be73cc4306cc549
Kavin Ranawella <kavin256@gmail.com>
2023-06-25 19:18:58 -0400

    adding css #15

--- src/screens/Calm/CalmHome.js
+++ src/screens/Calm/CalmHome.js
@@ -2,13 +2,15 @@ import React, { useContext } from "react";
 import { ScrollView } from "react-native";
 import SelectionTile from "../../components/SelectionTile";
 import themeContext from "../../../styles/themeContext";
+import {useGlobalContext} from "../../hooks/useGlobalContext";
 
 function CalmHome({navigation}) {
 
-    const theme = useContext(themeContext);
+    // const theme = useContext(themeContext);
+    const {theme} = useGlobalContext();
 
     return (
-        <ScrollView style={{backgroundColor:theme.backgroundColor}}>
+        <ScrollView style={{backgroundColor: theme.background}}>
             <SelectionTile name={"Change theme"} routeTo={'ScreenA'} navigation={navigation}/>
             <SelectionTile name={"Selection B"} routeTo={'ScreenB'} navigation={navigation}/>
         </ScrollView>
